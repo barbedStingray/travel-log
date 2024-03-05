2f908ef48daf354782b475d040e821754c6d86d3
Ben Rehmann <brehmann@me.com>
2024-03-05 15:42:12 -0600

    begin the travels

---
+++ src/LandingPage/LandingPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const LandingPage = () => {
+    return (
+        <div className='landingPage'>
+
+            <h1>The Travel Log</h1>
+            <div className='travelImage'></div>
+            <div className='travelImage'></div>
+            <div className='travelImage'></div>
+            <div className='travelImage'></div>
+
+        </div>
+    )
+}
+
+export default LandingPage;
